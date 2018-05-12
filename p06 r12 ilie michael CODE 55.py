5894ea878afcd321065ec94c055143331723df84
Michael Ilie <mcilieg@gmail.com>
2018-05-11 21:48:26 -0400

    Create p06 r12 ilie michael CODE 55.py

---
+++ p06 r12 ilie michael CODE 55.py
@@ -0,0 +1,9 @@
+coefficient = int(input("Enter the coefficient: "))
+power = int(input("Enter the power: "))
+num = coefficient * 10 ** power
+
+var = int(input("Enter wave length: "))
+if var < 3*10**9:
+  print("Radio Wave")
+elif var <3*10**12:
+  
