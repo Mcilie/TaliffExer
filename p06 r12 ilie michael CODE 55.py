52433b1d83a588dc234a32e53695e4a0044cd797
Michael Ilie <mcilieg@gmail.com>
2018-05-13 22:13:50 -0400

    Update p06 r12 ilie michael CODE 55.py

--- p06 r12 ilie michael CODE 55.py
+++ p06 r12 ilie michael CODE 55.py
@@ -1,9 +1,21 @@
 coefficient = int(input("Enter the coefficient: "))
-power = int(input("Enter the power: "))
+power = int(input("Enter the power of 10: "))
 num = coefficient * 10 ** power
 
-var = int(input("Enter wave length: "))
+var = num
 if var < 3*10**9:
   print("Radio Wave")
 elif var <3*10**12:
+  print("MicroWaves")
+elif var < 4.3 * 10**14:
+  print("Infrared Light")
+elif var < 7.5 * 10 ** 14:
+  print("Visible Light")
+elif var < 3 * 10 ** 17:
+  print("UltraViolet Light")
+elif var < 3 * 10 ** 19:
+  print("X-rays")
+else:
+  print("Gamma rays")
+  
   
