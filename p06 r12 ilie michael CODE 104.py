e1b173d25f2160453457f29e18c8b5cce37881df
Michael Ilie <mcilieg@gmail.com>
2018-05-23 22:12:22 -0400

    Create p06 r12 ilie michael CODE 104.py

---
+++ p06 r12 ilie michael CODE 104.py
@@ -0,0 +1,7 @@
+why = []
+q = int(input("Enter Element to enter to list (0 terminates program): "))
+while q!=0:
+  why.append(q)
+  q = int(input("Enter Element to enter to list (0 terminates program): "))
+for i in why: print(i)
+  
